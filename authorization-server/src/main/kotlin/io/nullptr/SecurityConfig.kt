70ecedb4af5d8a0631328a9b2c339d875b1b2580
imoyb <imoyb@qq.com>
2022-10-07 22:00:04 +0800

    authorization server init

---
+++ authorization-server/src/main/kotlin/io/nullptr/SecurityConfig.kt
@@ -0,0 +1,116 @@
+package io.nullptr
+
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    @Order(SecurityProperties.DEFAULT_FILTER_ORDER)
+    fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.exceptionHandling {
+            it.authenticationEntryPoint(LoginUrlAuthenticationEntryPoint("/login"))
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeRequests().anyRequest().authenticated()
+        http.formLogin()
+        http.httpBasic()
+        return http.build()
+    }
+
+    @Bean
+    fun registeredClientRepository() =
+        InMemoryRegisteredClientRepository(
+            RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oauth2-client")
+                .clientSecret("{bcrypt}\$2a\$10\$8DXL0t1aUDxUXkWPwd7syOWCtRlxpkMrvmLkA13R585eyWlap2GKK")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oauth2-client")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build()
+        )
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
+    @Bean
+    fun userDetailsService(): UserDetailsService =
+        InMemoryUserDetailsManager(
+            User("yc", "{noop}pnq123", emptyList()),
+            User("nullptr", "{noop}password@123", emptyList()),
+            User("ti19", "{noop}123456", emptyList()),
+            User(
+                "user",
+                "{bcrypt}\$2a\$10\$6rwMXarOJdcT30P/4i/pnOSdalwYSVSnZbFoWPW19CbmxmHuwuy16",
+                emptyList()
+            )
+        )
+
+    @Bean
+    fun providerSettings(): ProviderSettings = ProviderSettings.builder().build()
+
+    @Bean
+    fun jwkSource(): JWKSource<SecurityContext> {
+        val keyPair = generateRsaKey()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+        val rsaKey = RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+
+        val jwkSet = JWKSet(rsaKey)
+
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    private fun generateRsaKey() =
+        try {
+            val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+            keyPairGenerator.initialize(2048)
+            keyPairGenerator.generateKeyPair()
+        } catch (ex: Exception) {
+            throw IllegalStateException(ex)
+        }
+}
